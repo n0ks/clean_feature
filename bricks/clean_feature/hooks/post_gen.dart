85398c99c887e56c94fdd4852b87d25f6a8b4ac6
n0ks <rodrigocmp29@gmail.com>
2023-08-04 10:18:40 -0300

    add hooks

---
+++ bricks/clean_feature/hooks/post_gen.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import 'package:mason/mason.dart';
+
+Future<void> run(HookContext context) async {
+  final progress = context.logger.progress('Running hooks');
+
+  await Process.run('flutter',
+      ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+
+
+  progress.complete();
+}
