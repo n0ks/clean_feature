bbc0c62bbb6c804ab2170c47a8cce62ed3af9214
n0ks <rodrigocmp29@gmail.com>
2023-08-14 15:09:05 -0300

    run pub get before runner

--- bricks/clean_feature/hooks/post_gen.dart
+++ bricks/clean_feature/hooks/post_gen.dart
@@ -4,9 +4,12 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Running hooks');
 
-  await Process.run('flutter',
-      ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+  await Process.run('flutter', ['pub', 'get']);
 
+  await Process.run(
+    'flutter',
+    ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'],
+  );
 
   progress.complete();
 }
